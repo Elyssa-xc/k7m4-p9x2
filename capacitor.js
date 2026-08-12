db6c7addf752fbfa53d80acedb9acedf93d88c6b
Elyssa-xc <Elyssa-xc@users.noreply.github.com>
2026-08-13 05:11:30 +0800

    Deploy nuomiji frontend

---
+++ capacitor.js
@@ -0,0 +1,10 @@
+﻿window.Capacitor = window.Capacitor || {
+  isNativePlatform: () => false,
+  Plugins: {
+    Preferences: {
+      get: async ({ key }) => ({ value: localStorage.getItem(key) }),
+      set: async ({ key, value }) => { localStorage.setItem(key, value); },
+      remove: async ({ key }) => { localStorage.removeItem(key); }
+    }
+  }
+};
